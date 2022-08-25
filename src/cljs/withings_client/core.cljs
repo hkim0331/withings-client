a2652fc88b7dbde60bee9bffd1c07df7c35dae53
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-08-26 00:44:40 +0900

    0.4.11, Authorization: Bearer <access token>

--- src/cljs/withings_client/core.cljs
+++ src/cljs/withings_client/core.cljs
@@ -15,7 +15,7 @@
   (:import
    goog.History))
 
-(def ^:private version "0.4.11-SNAPSHOT")
+(def ^:private version "0.4.11")
 
 (defonce session (r/atom {:page :home
                           :name nil

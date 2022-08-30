3439cddd0d1008771844214fa0bcb4afddc92aef
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-08-30 19:02:31 +0900

    how to use .gitignore

--- src/cljs/withings_client/core.cljs
+++ src/cljs/withings_client/core.cljs
@@ -15,7 +15,7 @@
   (:import
    goog.History))
 
-(def ^:private version "0.6.10")
+(def ^:private version "0.6.11")
 
 (def redirect-uri js/redirectUrl)
 ;; (def redirect-uri "https://wc.melt.kyutech.ac.jp/callback")

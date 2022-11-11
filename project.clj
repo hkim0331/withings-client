19ec8041285b1114ccc3bf0c5e54a0ac9f7d0142
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-11-11 14:09:42 +0900

    line_id, class input

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject withings-client "0.11.4"
+(defproject withings-client "0.12.0"
 
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
--- src/cljs/withings_client/core.cljs
+++ src/cljs/withings_client/core.cljs
@@ -12,7 +12,7 @@
   (:import
    goog.History))
 
-(def ^:private version "0.11.4")
+(def ^:private version "0.12.0")
 
 ;; FIXME: better way?
 (def redirect-uri

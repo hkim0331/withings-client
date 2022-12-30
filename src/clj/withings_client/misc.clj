856ce3330695183fa4ae881019d865153271ad7d
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-12-30 11:24:25 +0900

    got. shoule remove nil parameters from query-params.

--- src/clj/withings_client/misc.clj
+++ src/clj/withings_client/misc.clj
@@ -17,12 +17,14 @@
    if lacked 'hh:mm:ss' part, redo supplying '00:00:00'.
    Return value is seconds from epoch. integer."
   [s]
-  (log/info "datetime->second s" s)
+  ;; noisy
+  ;; (log/info "datetime->second s" s)
   (when (seq s)
     (let [[date time] (str/split s #" ")]
       (if (seq time)
         (-> (str date "T" time)
-            jt/to-sql-timestamp ;; jt/sql-timestamp
+            ;; was jt/to-sql-timestamp
+            jt/sql-timestamp
             jt/to-millis-from-epoch
             (quot 1000))
         (datetime->second (str date " 00:00:00"))))))

0fc14262a1e8056dcdada48e5b4265e65b460598
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2023-09-22 23:32:29 +0000

    0.21.1

--- bump-version.sh
+++ bump-version.sh
@@ -21,8 +21,8 @@ else
     SED="/usr/bin/sed -E"
 fi
 
-${SED} -i "s|(defproject \S+)|\1 \"$1\"|" project.clj
+${SED} -i "s|(defproject \S+).*|\1 \"$1\"|" project.clj
 
 #${SED} -i "s|(def \^:private version) .+|\1 \"$1\")|" src/core.clj
 
-${SED} -i "s|(def \^:private version)|\1 \"$1\")|" src/cljs/withings_client/core.cljs
+${SED} -i "s|(def \^:private version).*|\1 \"$1\")|" src/cljs/withings_client/core.cljs
